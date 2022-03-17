aeea40e7a9d71e1f238e6bd3d23c1790ea727089
mbehadir <mustafabehadir@gmail.com>
2022-03-17 10:24:59 +0300

    port-configs is moved from tenant to site

--- packages/cisco-dc/python/cisco_dc/utils.py
+++ packages/cisco-dc/python/cisco_dc/utils.py
@@ -57,7 +57,7 @@ def get_description(port, port_parameters):
 
     """
     tenant = port._parent._parent._parent.name
-    description = f"{tenant}_{port_parameters['name']}_{port_parameters['mode']}_{port_parameters['speed']}".upper(
+    description = f"{port_parameters['name']}_{port_parameters['mode']}_{port_parameters['speed']}".upper(
     )
     if port_parameters['type'] == 'ethernet':
         return f"{description}_ETH"
@@ -111,49 +111,6 @@ def get_vpc_nodes(root, port):
     site = root.cisco_dc__dc_site[port.site]
     node_group = site.node_group[port.vpc_port_channel.node_group]
     return node_group.node_1, node_group.node_2
-
-
-def get_node_group(root, port, device):
-    """Function to return vpc node group for a given vpc node
-
-    Args:
-        root: Maagic object pointing to the root of the CDB
-        port: service node
-        device: node name
-
-    Returns:
-        Integer node-group
-
-    """
-    trans = ncs.maagic.get_trans(root)
-    query_path = f"/vxlandc-core:vxlandc/vxlandc-core:sites/vxlandc-core:site[vxlandc-core:fabric='{port.site}']/vxlandc-core:node-group"
-    with Query(trans, query_path, '/', ['id', 'node-1', 'node-2'], result_as=ncs.QUERY_STRING) as q:
-        for r in q:
-            if device in (r[1], r[2]):
-                return int(r[0])
-
-
-def get_port_channel_id_pool_name(root, port):
-    """Function to return port-channel id pool name for a given node
-
-    Args:
-        root: Maagic object pointing to the root of the CDB
-        port: service node
-
-    Returns:
-        String port-channel id pool if port type is port-channel or vpc-port-channel else None
-
-    """
-    if port.port_type == 'port-channel':
-        site = root.cisco_dc__dc_site[port.site]
-        device = port.port_channel.node
-        po_id_pool = site.node[device].po_id_pool if site.node[device].node_type.string == 'standalone' else site.node_group[get_node_group(
-            root, port, device)].po_id_pool
-    elif port.port_type == 'vpc-port-channel':
-        site = root.cisco_dc__dc_site[port.site]
-        device_group = port.vpc_port_channel.node_group
-        po_id_pool = site.node_group[device_group].po_id_pool
-    return po_id_pool
 
 
 def is_node_vpc(root, port, port_parameters):

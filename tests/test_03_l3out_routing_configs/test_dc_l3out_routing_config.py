233992a11ddb95b7c8964ccb05a7c0856f11c207
mbehadir <mustafabehadir@gmail.com>
2022-05-08 23:58:10 +0300

    tests are ficed.

--- tests/test_03_l3out_routing_configs/test_dc_l3out_routing_config.py
+++ tests/test_03_l3out_routing_configs/test_dc_l3out_routing_config.py
@@ -113,23 +113,6 @@ class L3OutRoutingConfigsTests:
         assert json.loads(get_resp.text) == expected
 
     @mark.parametrize('expected, patch_path, get_path', [
-        (expected_path / 'ref_007_l3out_bgp_config_sw_1.json',
-         'cisco-dc:dc-site=avr-dss1-lbox-yaani-fabric/tenant-service=0001_TURKCELL/bridge-domain=BD-SERVICE-1/routing/bgp=10.0.0.2',
-         'tailf-ncs:devices/device=nw_lf_cnx9_001.dsslab_site1/config/tailf-ned-cisco-nx:router/bgp=65001/vrf=tcell-grt/neighbor=10.0.0.2'),
-        (expected_path / 'ref_007_l3out_bgp_config_sw_2.json',
-         'cisco-dc:dc-site=avr-dss1-lbox-yaani-fabric/tenant-service=0001_TURKCELL/bridge-domain=BD-SERVICE-1/routing/bgp=10.0.0.2',
-         'tailf-ncs:devices/device=nw_lf_cnx9_002.dsslab_site1/config/tailf-ned-cisco-nx:router/bgp=65001/vrf=tcell-grt/neighbor=10.0.0.2'),
-    ], indirect=['expected'])
-    def test_006_l3out_fabric_internal_bgp_weight(self, expected, patch_path, get_path):
-        patch_resp = self.nso.patch(
-            payload=self.payload_path / 'test_007_config.json', path=patch_path, params='')
-        get_resp = self.nso.get(path=get_path)
-        data = json.loads(get_resp.text)
-        assert patch_resp.status_code == 204
-        assert get_resp.status_code == 200
-        assert data == expected
-
-    @mark.parametrize('expected, patch_path, get_path', [
         (expected_path / 'ref_006_l3out_bgp_config_sw_1.json',
          'cisco-dc:dc-site=avr-dss1-lbox-yaani-fabric/tenant-service=0001_TURKCELL/bridge-domain=BD-SERVICE-1/routing/bgp=10.0.0.2',
          'tailf-ncs:devices/device=nw_lf_cnx9_001.dsslab_site1/config/tailf-ned-cisco-nx:router/bgp=65001/vrf=tcell-grt/neighbor=10.0.0.2'),
@@ -137,9 +120,26 @@ class L3OutRoutingConfigsTests:
          'cisco-dc:dc-site=avr-dss1-lbox-yaani-fabric/tenant-service=0001_TURKCELL/bridge-domain=BD-SERVICE-1/routing/bgp=10.0.0.2',
          'tailf-ncs:devices/device=nw_lf_cnx9_002.dsslab_site1/config/tailf-ned-cisco-nx:router/bgp=65001/vrf=tcell-grt/neighbor=10.0.0.2'),
     ], indirect=['expected'])
-    def test_007_l3out_fabric_internal_bgp_password(self, expected, patch_path, get_path):
+    def test_006_l3out_fabric_internal_bgp_weight(self, expected, patch_path, get_path):
         patch_resp = self.nso.patch(
             payload=self.payload_path / 'test_006_config.json', path=patch_path, params='')
+        get_resp = self.nso.get(path=get_path)
+        data = json.loads(get_resp.text)
+        assert patch_resp.status_code == 204
+        assert get_resp.status_code == 200
+        assert data == expected
+
+    @mark.parametrize('expected, patch_path, get_path', [
+        (expected_path / 'ref_007_l3out_bgp_config_sw_1.json',
+         'cisco-dc:dc-site=avr-dss1-lbox-yaani-fabric/tenant-service=0001_TURKCELL/bridge-domain=BD-SERVICE-1/routing/bgp=10.0.0.2',
+         'tailf-ncs:devices/device=nw_lf_cnx9_001.dsslab_site1/config/tailf-ned-cisco-nx:router/bgp=65001/vrf=tcell-grt/neighbor=10.0.0.2'),
+        (expected_path / 'ref_007_l3out_bgp_config_sw_2.json',
+         'cisco-dc:dc-site=avr-dss1-lbox-yaani-fabric/tenant-service=0001_TURKCELL/bridge-domain=BD-SERVICE-1/routing/bgp=10.0.0.2',
+         'tailf-ncs:devices/device=nw_lf_cnx9_002.dsslab_site1/config/tailf-ned-cisco-nx:router/bgp=65001/vrf=tcell-grt/neighbor=10.0.0.2'),
+    ], indirect=['expected'])
+    def test_007_l3out_fabric_internal_bgp_password(self, expected, patch_path, get_path):
+        patch_resp = self.nso.patch(
+            payload=self.payload_path / 'test_007_config.json', path=patch_path, params='')
         get_resp = self.nso.get(path=get_path)
         data = json.loads(get_resp.text)
         data['tailf-ned-cisco-nx:neighbor'][0]['password']['passwd'] = ".*"
